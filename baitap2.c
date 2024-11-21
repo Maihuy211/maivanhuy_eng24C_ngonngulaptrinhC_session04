b3c5aca8b2686f665765d99894bab0db508192a7
Maihuy211 <maihuylc@gmail.com>
2024-11-22 01:23:07 +0700

    upload baitap2

---
+++ baitap2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (){
+	int number;
+	printf("moi ban nhap mot so: ");
+	scanf("%d",&number);
+	if(number%2==0){
+		printf("la so chan");
+		}
+	else{
+		printf("la so le");
+	}
+	return 0;
+	}
